7e67335f057dc76be0544f86b3d19fc4bfb64a72
Vaibhav Patel <patel.vaibhav@gmail.com>
2012-10-18 07:04:30 -0700

    Initial Commit

---
+++ Happiness/HappinessViewController.h
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by vaibhav patel on 10/18/12.
+//  Copyright (c) 2012 vaibhav patel Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessViewController : UIViewController
+
+@end
---
+++ Happiness/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Happiness
+//
+//  Created by vaibhav patel on 10/18/12.
+//  Copyright (c) 2012 vaibhav patel Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HappinessAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HappinessAppDelegate class]));
+    }
+}
